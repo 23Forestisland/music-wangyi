9df7d8c10cd882063243483bb8b9badaa53d81e1
张鸿宇 <739185356@qq.com>
2024-12-18 19:33:08 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createSSRApp } from "vue";
-<<<<<<< HEAD
 import { createPinia } from 'pinia'
 import './common/icon/font_4781276_vaj6ayh9ur/iconfont.css'
 import './common/scss/common.scss'
-=======
->>>>>>> bdcd9615e3fd3b3304f3b6fc58379b0c739d6fd7
 import App from "./App.vue";
 
 export function createApp() {
